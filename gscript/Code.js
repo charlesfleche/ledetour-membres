55f0df9d5df26d0194759cac64b5de24df1622b9
Charles Flèche <charles.fleche@free.fr>
2020-08-08 19:30:45 -0400

    Fetch lastName,firstName and phone

--- gscript/Code.js
+++ gscript/Code.js
@@ -27,6 +27,9 @@ function makeTableObject(multiArray){
   const STATUS_COL = 0;
   const FAM_COL = 2;
   const MEMBER_COL = 3;
+  const LASTNAME_COL = 5;
+  const FIRSTNAME_COL = 6;
+  const PHONE_COL = 9;
   const INDI_HRS_COL = 21;
   const FAM_HRS_COL = INDI_HRS_COL + 1;
 
@@ -36,6 +39,9 @@ function makeTableObject(multiArray){
 
   multiArray.forEach(function(row, i){
 
+      var lastName = row[LASTNAME_COL];
+      var firstName = row[FIRSTNAME_COL];
+      var phone = row[PHONE_COL];
       var status = (row[STATUS_COL] == "Actif");
       var member = row[MEMBER_COL];
       var family = row[FAM_COL];
@@ -52,6 +58,9 @@ function makeTableObject(multiArray){
       }
 
     tableObject.members[row[MEMBER_COL]] = {
+        lastName: lastName,
+        firstName: firstName,
+        phone: phone,
         active: status,
         hours_in_bank: hours
       };
