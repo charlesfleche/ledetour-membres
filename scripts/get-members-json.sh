7695441bf2f07287bc8dd8972d0cf13f9b8e1b90
Mau <30912246+mauedu@users.noreply.github.com>
2021-08-30 21:01:26 -0400

    change deployment URL

--- scripts/get-members-json.sh
+++ scripts/get-members-json.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DST=$1
-SRC=https://script.google.com/macros/s/AKfycbwYoR0gLZGJDd-6cJExE5oPQMcArmVKqNNyV1a4FV_kl5--BfVV5N-bbB0T014nY3mO/exec
+SRC=https://script.google.com/macros/s/AKfycbxn8qstSRiod-92Op2hSUACZW3tbsa3ytdu4X8JbykXfFQ9VUBU2tuXCdHUKVIt8z3dkg/exec
 
 LOG=/tmp/get-members-json.log
 
