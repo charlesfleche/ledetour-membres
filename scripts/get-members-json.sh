c2493b20b68fef9c2d52d9a434fbbb60d091e099
Charles Flèche <charles.fleche@free.fr>
2024-03-31 14:23:41 -0400

    Fix default .json paths

--- scripts/get-members-json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DST=$1
-SRC=https://script.google.com/macros/s/AKfycbxn8qstSRiod-92Op2hSUACZW3tbsa3ytdu4X8JbykXfFQ9VUBU2tuXCdHUKVIt8z3dkg/exec
-
-LOG=/tmp/get-members-json.log
-
-wget --output-document=$DST --output-file=$LOG $SRC
